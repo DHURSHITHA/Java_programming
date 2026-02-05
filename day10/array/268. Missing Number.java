385f6daf746782621908939947c5add8f27d1257
DHURSHITHA N <dhurshitha.n2006@gmail.com>
2026-02-05 21:50:27 +0530

    Add solution for 'Missing Number' problem

---
+++ day10/array/268. Missing Number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum=0;
+        int tot=(nums.length*(nums.length+1))/2;
+        for(int num:nums)
+        {
+            sum+=num;
+        }
+        return tot-sum;
+    }
+}
