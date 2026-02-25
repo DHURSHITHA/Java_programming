2ec6b13f7e5a02f30c3102e32cbd720124a474ba
DHURSHITHA N <dhurshitha.n2006@gmail.com>
2026-02-25 21:19:44 +0530

    Create 153.  Findd minimum in rotated sorted array.java

---
+++ day10/array/153.  Findd minimum in rotated sorted array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMin(int[] nums) {
+    int low=0, high =nums.length - 1;
+    int mid;
+    
+    while(low < high)
+    {
+        mid = low + (high - low)/2; // 2
+        
+        if (nums[mid] > nums[high])
+            low = mid + 1;
+        else
+            high = mid;
+    }
+    
+    return nums[low];
+        
+    }
+}
