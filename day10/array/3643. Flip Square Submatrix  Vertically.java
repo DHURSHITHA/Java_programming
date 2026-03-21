59793e8909a211aa46728467ed1c49e2a0f17ddc
DHURSHITHA N <dhurshitha.n2006@gmail.com>
2026-03-21 17:32:53 +0530

    Add method to reverse square submatrix vertically

---
+++ day10/array/3643. Flip Square Submatrix  Vertically.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+
+        for (int j = y; j < y + k; j++) {
+
+            for (int i = x; i < x + k / 2; i++) {
+
+                int temp = grid[i][j];
+
+                grid[i][j] = grid[x + k - 1 - (i - x)][j];
+
+                grid[x + k - 1 - (i - x)][j] = temp;
+            }
+        }
+
+        return grid;
+    }
+}
